3c9e6913620e14d290f1e4c21e745b320e9bc3c9
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-05 18:51:04 +0200

    customized data

---
+++ src/ProductTable.java
@@ -0,0 +1,45 @@
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+public class ProductTable extends JFrame
+{
+    public ProductTable()
+    {
+        //headers for the table
+        String[] columns = new String[] {
+                "Product Name", "Product Description", "Points", "Price", "Quantity", "Action"
+        };
+
+        //actual data for the table in a 2d array
+        Object[][] data = new Object[][] {
+                {"Donut", "for Eating", 40.0, 100, 243, false},
+                {"Donut", "for Eating", 40.0, 100, 243, false},
+                {"Donut", "for Eating", 40.0, 100, 243, false},
+                {"Donut", "for Eating", 40.0, 100, 243, false},
+                {"Donut", "for Eating", 40.0, 100, 243, false},
+                {"Donut", "for Eating", 40.0, 100, 243, false},
+                {"Donut", "for Eating", 40.0, 100, 243, false}
+        };
+        //create table with data
+        JTable table = new JTable(data, columns);
+
+        //add the table to the frame
+        this.add(new JScrollPane(table));
+
+        this.setTitle("Table Example");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ProductTable();
+            }
+        });
+    }
+}

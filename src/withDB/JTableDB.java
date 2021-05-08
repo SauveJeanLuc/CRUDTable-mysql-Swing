64128036859257575de08cc307ae9f6d14d62a94
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-08 16:49:09 +0200

    Main class included

---
+++ src/withDB/JTableDB.java
@@ -0,0 +1,10 @@
+package withDB;
+
+public class JTableDB {
+    //MAIN CLASS
+    public static void main(String[] args) {
+        //CALL UPDATER CLASS WHICH IS OUR GUI CLASS
+        new Updater().setVisible(true);
+    }
+
+}
